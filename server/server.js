2fe0c3c129d8524558da8931b94441259244018a
Fleshren <fleshren1@gmail.com>
2024-03-18 02:39:17 -0500

    tweaks

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
