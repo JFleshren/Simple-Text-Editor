04092f5256b8ad7826a5229a050d01f0db2a21ae
Fleshren <fleshren1@gmail.com>
2024-03-18 13:38:01 -0500

    aasd

--- server/server.js
+++ server/server.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const path = require('path');
-const EventEmitter = require('events');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const emitter = new EventEmitter();
-emitter.setMaxListeners(20);
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
